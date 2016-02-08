40c9255d331753f590c475d7ebc2311a9e4faebb
Fares Alaboud <fares.alaboud@gmail.com>
2016-02-08 00:08:14 +0000

    Fix with missing quotation mark.

--- run.rb
+++ run.rb
@@ -5,7 +5,7 @@ File.open("list.txt", 'w') {|f| f.write(Time.now.getutc) }
 
 emails.each do |email| 
 	verificationCode = (0...10).map { ('A'..'Z').to_a[rand(26)] }.join
-	File.open("list.txt", 'a') {|f| f.write("\n#{verificationCode}) }
+	File.open("list.txt", 'a') {|f| f.write("\n#{verificationCode}") }
 
 	message = <<MESSAGE_END
 From: sender <sender@email.com>
